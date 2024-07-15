63ce741f42a8285b83b8de57d06767294867ed27
jouraqasim <145996683+jouraqasim@users.noreply.github.com>
2024-07-15 14:46:14 +0500

    Add files via upload

---
+++ Logic gates.py
@@ -0,0 +1,31 @@
+def AND(A, B):
+    return A & B
+print("Table of AND function")
+print("----------------------")
+print("My output 0 AND 0 is",AND(0,0))
+print("My output 0 AND 1 is",AND(0,0))
+print("My output 1 AND 0 is",AND(0,0)) 
+print("My output 1 AND 1 is",AND(1,1))
+print("----------------------") 
+print()
+
+def OR(A, B):
+    return A | B
+print("Table of OR function")
+print("********************") 
+print("output is 0 OR 1 =",OR(1,1))
+print("output is 0 OR 0 =",OR(0,0))
+print("output is 1 OR 1 =",OR(1,1))
+print("output is 1 OR 0 =",OR(1,1))
+print("********************")
+print()
+
+def NOT(A):
+    return A
+print("Table of NOT function")
+print("---------------------")
+print("output of 1 is =",NOT(0))
+print("output of 0 is =",NOT(1))
+print("---------------------")
+
+
